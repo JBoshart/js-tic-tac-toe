5e6c3acbca0e391ccd08ccbc6279f3d926576513
Jillian Boshart <Jilliliz@gmail.com>
2016-06-30 16:19:14 -0700

    added mousdown function for buttons. When pressed the button calls the 'makePlay' function of TicTacToe, which checks to see if that space has been played and updates value within board if not.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,29 +1,36 @@
-function TicTacToe() {
+function TicTacToe(callback) {
   this._player_X = 'X'
   this._player_O = 'O'
   this._current_player = this._player_X
   this._gameboard = {
-    top_row: [[],[],[]]
-    middle_row: [[],[],[]]
-    bottom_row: [[],[],[]]
+    'top_row': [[],[],[]],
+    'middle_row': [[],[],[]],
+    'bottom_row': [[],[],[]]
   }
-
-// This is eventually going to be a 'start new game' or reset button.
-  // this.newGame = function(event) {
-  //   new TicTacToe
-  // }
-
-  // how are win conditions stored? Are they stored?
-
 }
 
-TicTacToe.prototype.makePlay = function() {
-  // finds button selected
-  // finds current player
-  // inputs correct letter (X or O) into correct gameboard storage
-  // calls win? function
-  // switches current player if game is not a win or Cat's Game
+TicTacToe.prototype.makePlay = function(spot) {
+  var board = this._gameboard
+  var row = spot.charAt(0)
+  var col = spot.charAt(1)
+
+  if (row === 't') {
+    if (this._gameboard['top_row'][0][col] === undefined) {
+      this._gameboard['top_row'][0][col] = this._current_player
+    }
+  } else if (row === 'm') {
+    if (this._gameboard['middle_row'][0][col] === undefined) {
+      this._gameboard['middle_row'][0][col] = this._current_player
+    }
+  } else if (row === 'b') {
+    if (this._gameboard['bottom_row'][0][col] === undefined) {
+      this._gameboard['bottom_row'][0][col] = this._current_player
+    }
+  }
+  //   // calls win? function
+  //   // switches current player if game is not a win or Cat's Game
 }
+
 
 TicTacToe.prototype.hasWon = function() {
   // checks gameboard in all combinations to see if win condition has been met. (_NOTE: Probably only needs to check win conditions that contain the current square being played. Checking all combinations is redundant work.)
@@ -33,25 +40,12 @@ TicTacToe.prototype.hasWon = function() {
     // How to handle Cat's Game? Should the function call Cat's Game after all possible plays have been made (meaning, all 9 squares are assigned to X or O), OR after it sees that it is not possible for either player to win (which you can see after 7 turns have been played? I think?)
 }
 
+$(document).ready(function() {
 
-// Things to think about re: What constitutes a turn?
-  // Computer must determine which player's turn it is
-  // Select a space
-    // Is a space available?
-    // Is the space adventagous?
-  // Claim space using player identifier
-  // Check win/loss/draw state
-    // what is a win?
-    // what is a draw?
-    // what is a loss?
-    // comminucate outcome
-  // continue play?
+  var ttt = new TicTacToe
 
-  $(document).ready(function() {
-
-    var ttt = new TicTacToe
-
-
-  // For eventual newgame/reset funcionality discribed above.
-    // $('.game-control .new-game').on('mousedown', newGame)
+  $('.play').mousedown(function(event) {
+    var button = $(this).data('grid')
+    ttt.makePlay(button)
   })
+})
